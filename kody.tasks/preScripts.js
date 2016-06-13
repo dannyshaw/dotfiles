f849f944cdc21bfee0ea43629aa51e03216f2fbd
Danny Shaw <code@dannyshaw.io>
2016-06-13 17:20:09 +1000

    added Fuzzy File matcher

---
+++ kody.tasks/preScripts.js
@@ -0,0 +1,17 @@
+
+/**
+  * Run pre installation scripts
+  * @license MIT
+  * @author dannyshaw
+*/
+const options = {
+    name: 'pre scripts',
+    description: 'run pre installation scripts',
+    exec: function(resolve, reject, shell, log, config) {
+      const script = config.scripts.pre;
+      shell.exec(`sh ${script}`);
+      resolve();
+    }
+  };
+
+exports.options = options;
--- shellScripts/post.sh
+++ shellScripts/post.sh
@@ -1,4 +1,10 @@
 echo "Running Post Installation Script"
 
+# install Fuzzy File Search
+if [ ! -d "$HOME/.fzf" ]; then
+  git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
+  $HOME/.fzf/install
+fi
+
 
 echo "Post Installation Script Complete"
