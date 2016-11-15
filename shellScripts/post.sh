9d276fbd451dbd116238065799ce1fe5adc2802d
Danny Shaw <code@dannyshaw.io>
2016-11-15 20:17:25 +1100

    adding sublime packages

--- shellScripts/post.sh
+++ shellScripts/post.sh
@@ -6,5 +6,9 @@ if [ ! -d "$HOME/.fzf" ]; then
   $HOME/.fzf/install
 fi
 
+#symlink sublime text packages
+rm -rf $HOME/.config/sublime-text-3/Packages/User
+ln -s $HOME/.dotfiles/sublime/User $HOME/.config/sublime-text-3/Packages/User
+
 
 echo "Post Installation Script Complete"
