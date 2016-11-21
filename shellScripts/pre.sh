befd25b96d5b51eb3c3a96286a3afbb50b3ea175
Danny Shaw <code@dannyshaw.io>
2016-11-21 12:29:09 +1100

    some more deps

--- shellScripts/pre.sh
+++ shellScripts/pre.sh
@@ -2,7 +2,7 @@
 
  echo "Running Pre installation script"
 
-sudo apt install python-pip python3-dev python3-pip
+sudo apt install python-pip python-dev python3-dev python3-pip
 wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python
 wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python3
 
