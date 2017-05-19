a12d59780a9508fe7e4b7440bee1d9659e6f7d56
Danny Shaw <code@dannyshaw.io>
2017-05-19 10:22:49 +1000

    sublime and added Xresources

--- shellScripts/pre.sh
+++ shellScripts/pre.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
- echo "Running Pre installation script"
-
-sudo apt install python-pip python-dev python3-dev python3-pip
+ echo "Running Pre installation script" 
+sudo apt-get install -y python-pip python-dev python3-dev python3-pip
 wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python
 wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python3
 
