199beeef2fac52e85a5b624ca3e5dfef73012aca
Danny Shaw <code@dannyshaw.io>
2017-10-17 07:27:53 +1100

    sublime and some kody changes

--- shellScripts/post.sh
+++ shellScripts/post.sh
@@ -6,7 +6,7 @@ echo "Running Post Installation Script"
 
 # install oh my zsh
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
-  git clone clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+  git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 fi
 
 # install Fuzzy File Search
